0ca524ff44edcdbf24e7fedd955c48591201c85e
Ephantus2 <ephantusmwago6@gmail.com>
2026-03-24 11:41:27 +0300

    links

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,9 +4,21 @@ const Page = () => {
     <>
     <Link href="/">Home</Link>
     <h1>Product list</h1>
-    <h1>product 1</h1>
-    <h1>product 2</h1>
-    <h1>product 3</h1>
+    <h1>
+      <Link href="/products/1">
+      product 1
+      </Link>
+      </h1>
+    <h1>
+      <Link href="/products/2">
+      product 2
+      </Link>
+      </h1>
+    <h1>
+      <Link href="/products/3">
+      product 3
+      </Link>
+      </h1>
     </>
   )
 }
