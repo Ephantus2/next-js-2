e8cfa4f3af2244daaff6b4e2d929c9e7165892fb
Ephantus2 <ephantusmwago6@gmail.com>
2026-03-24 13:13:56 +0300

    error.tsx

--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,20 @@
 
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+   return Math.floor(Math.random() * count);
+}
+
 const Page = async ({params}: {
     params: Promise<{productId: string; reviewId: string}>
 }) => {
+
+    const random = getRandomInt(2);
+
+    if(random === 1){
+        throw new Error("Error loading resource")
+    }
+
     const {productId, reviewId} = await params
 
     if (parseInt(reviewId) > 1000){
